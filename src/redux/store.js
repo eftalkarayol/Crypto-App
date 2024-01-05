553bfc5dcf66c4319554f012e4b80aa221559be8
eftalkarayol <eftal-karayol@hotmail.com>
2024-01-05 14:40:53 +0300

    coin info add

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import { thunk } from "redux-thunk";
 import coinListReducer from "./reducers/coinListReducer";
+import coinInfoReducer from "./reducers/coinInfoReducer";
 
 const rootReducer = combineReducers({
   coinListReducer,
+  coinInfoReducer,
 });
 
 export default createStore(rootReducer, applyMiddleware(thunk));
